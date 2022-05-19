baf002087bde0bae7c915c7112ed6ecfa6a4a015
Jose-Gomez-C <josemania10@gmail.com>
2022-05-18 20:37:10 -0500

    files needed to dockerized the app

--- src/main/java/adidas/dassplit/DasSplitApplication.java
+++ src/main/java/adidas/dassplit/DasSplitApplication.java
@@ -3,7 +3,6 @@ package adidas.dassplit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
--- src/test/java/adidas/dassplit/DasSplitApplicationTests.java
+++ src/test/java/adidas/dassplit/DasSplitApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DasSplitApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
